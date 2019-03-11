e3f59bfce4bf54e3a8c42611006046c480835f33
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-03-11 18:51:40 -0400

    Adding in the user model and adding in the routes for registering and logging in the user

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: '../.env' });
+
+// Grabbing the models to use from mongoose
+const User = require('./models/User');
 
 // Initializing the express app
 const app = express();
@@ -11,16 +15,91 @@ const app = express();
 const PORT = process.env.PORT || 4444;
 
 // Connecting to the mongoose database
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error('Error connecting to database: ', err));
 
 // Initializing the middelware to use to express
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send({ ok: 'OK' });
+// Production mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+}
+
+/**
+ * Request for the users
+ */
+
+// Route to register the user to the database
+app.post('/api/users/register', (req, res) => {
+    // Storing the new user from the req.body;
+    const user = new User(req.body);
+
+    // Saving the user to the database
+    user.save((err, doc) => {
+        // If there is an error then we are sending back the error message
+        if (err) return res.json({ success: false, err });
+
+        // If no error then we are sending back the registered user
+        res.status(200).json({ success: true, userData: doc });
+    });
 });
+
+// Route to login the user from the database
+app.post('/api/users/login', (req, res) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email }, (err, user) => {
+        // If there is an error we are sending back the error
+        if (err) return res.json({ loginSuccess: false, err });
+
+        // If there is no user then we need to let them know that the email was not found
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: 'Email not found'
+            });
+
+        // We need to compare the password that was entered to the one in the database
+        user.comparePassword(password, (err, isMatch) => {
+            // If there is an error we are returning the password error
+            if (err) return res.json({ loginSuccess: false, err });
+
+            // If the password doesn't match we need to tell the user the password is incorrect
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: 'Invalid Password'
+                });
+
+            // Generating the token for the user when logging in
+            user.generateToken((err, user) => {
+                // If there is an error we need to send a status of 400 and then send the error
+                if (err) return res.status(400).send(err);
+
+                /**
+                 * If there is no error then we need to store the token as a cookie
+                 * @param { name } string version of the name of the cookie ( this name can be whatever you want )
+                 * @param { value } string version of the token that is being sent back from the user
+                 */
+                res.cookie('w_auth', user.token)
+                    .status(200)
+                    .json({ loginSuccess: true });
+            });
+        });
+    });
+});
+
+// If we are in production mode then we will serve the index.html file that is made from the build directory
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/build/index.html'));
+    });
+}
 
 // Starting the express server on the specified port
 app.listen(PORT, (req, res) => {
---
+++ server/models/User.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Schema = mongoose.Schema;
+require('dotenv').config({ path: '../../.env' });
+
+// Creating the Users Schema
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    firstName: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    token: {
+        type: String
+    }
+});
+
+// Hash the password so if someone has access to database it can't be seen
+UserSchema.pre('save', function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            this.password = hash;
+            next();
+        });
+    });
+});
+
+// Need to compare the passwords when the user is trying to login
+UserSchema.methods.comparePassword = function(userPassword, callback) {
+    bcrypt.compare(userPassword, this.password, function(err, isMatch) {
+        // If there is an error then we are passing the error to the callback
+        if (err) return callback(err);
+
+        // We are passing the isMatch to the callback letting the server know if there is a match or not in passwords
+        callback(null, isMatch);
+    });
+};
+
+// Need to generate the user token when they are authenticated
+UserSchema.methods.generateToken = function(callback) {
+    const user = this;
+    /**
+     * Signing the JWT token to give to the user
+     * @param { user._id } string ID of the user in the database
+     * @param { SECRET } string Secret supplied in the .env files as a secret for no one else to know
+     */
+    const token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+
+    user.token = token;
+    user.save(function(err, user) {
+        // If there is an error saving the token then we pass the error back
+        if (err) return callback(err);
+
+        // If there is not an error saving the token then we pass back the user
+        callback(null, user);
+    });
+};
+
+// Exporting the Users Sceham as a mongoose model
+module.exports = mongoose.model('User', UserSchema);
